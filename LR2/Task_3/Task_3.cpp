a4c14b50d4e8a7a59eb2a2fc4483bbaa2e719853
Arseniy Finevich <finevich03@gmail.com>
2025-09-26 14:08:07 +0300

    added 4 labs

---
+++ LR2/Task_3/Task_3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double b1;
+    cout << "Введите 1 член геометрической прогрессии" << endl;
+    cin >> b1; 
+    cout << "Сумма: " << b1/(1-1.0/28) << endl;
+
+    return 0;
+}
