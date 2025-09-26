2ad024a0673b301ecb3d6135ff9a5a480ede4abd
Arseniy Finevich <finevich03@gmail.com>
2025-09-26 17:11:12 +0300

    lab1 orig edited

--- LR1/Task_3.cpp
+++ LR1/Task_3.cpp
@@ -13,6 +13,4 @@ int main() {
         double y = sin(x); // функция y = sin(x)
         std::cout << x << " | " << y << std::endl; // Выводим значения функции для каждого аргумента (x | y)
     }
-
-    return 0;
 }
