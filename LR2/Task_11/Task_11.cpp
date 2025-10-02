e1dc3a6c6ed48db214d313dbf7909bdb6a268fd6
Arseniy Finevich <finevich03@gmail.com>
2025-10-02 21:23:27 +0300

    added Task_11

---
+++ LR2/Task_11/Task_11.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int h1, m1, h2, m2;
+    cout << "Введите h1, m1, h2, m2 соответственно" << endl;
+    cin >> h1 >> m1 >> h2 >> m2;
+
+    int h_m = (h2*60 + m2 - (h1*60 + m1));
+    int h = h_m / 60;
+    int m = h_m % 60;
+
+    cout << "Студент решал задачи " << h << " часов " << m << " минут" << endl; 
+}
