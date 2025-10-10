b6c3993bae073c76af9a5de80d5b16a5fd858fa4
Arseniy Finevich <clumserinri537@gmail.com>
2025-10-10 11:18:02 +0300

    added LR3(2)

--- LR3/main.cpp
+++ LR3/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <limits>
-#include "Task_1.h"
 
+#include "Task_1.h"
+#include "Task_2.h"
+
+#define SEP cout << "************************" << endl;
+#define PRE_ESSENCE "Суть задания:\n "
 #define COMPL_BY "\n Выполнил: Финевич Арсений"
 
 using namespace std;
@@ -18,6 +22,7 @@ int main () {
 int select_choice () {
     int choice;
     cin >> choice;
+    SEP
     if (!cin.fail()) {
         return choice;
     } else {
@@ -29,30 +34,37 @@ int select_choice () {
 
 void menu () {
     vector<string> essence = {
-        "Суть задания:\n Используя цикл while вычислить сумму (от i = 1 до i = 30) N(i) = (ai - bi)^2, где ai и bi определяются по определенной формуле"
+        "Используя цикл while вычислить сумму (от i = 1 до i = 30) N(i) = (ai - bi)^2, где ai и bi определяются по определенной формуле",
+        "Используя цикл do while найти сумму ряда с точностью 𝜀 = 10^(-3), di = 1/(2^n) + 1/(3^n)"
     };
     bool exit = false;
     while (!exit) {
         int selected_choice;
-        cout << "Выберите пункт меню (число):\n 0. Выйти из программы\n 1. Выполнить задание 1" << endl;
+        cout << "Выберите пункт меню (число):\n -> 0. Выйти из программы\n -> 1. Используя цикл while, вычислить N (Задание 1)\n -> 2. Используя цикл do while найти сумму ряда с точностью" << endl;
         selected_choice = select_choice();
-        cout << selected_choice << endl;
 
         switch (selected_choice) {
         case -1:
             cout << "Ошибка. Введите ЧИСЛО, соответствующее пункту меню!!!" << endl;
+            SEP
             break;
 
         case 0:
             exit = true;
             break;
         case 1:
-            cout << essence[0] << COMPL_BY << endl;
+            cout << PRE_ESSENCE << essence[0] << COMPL_BY << endl;
             do_Task_1();
+            SEP
             break;
-            
+        case 2:
+            cout << PRE_ESSENCE << essence[1] << COMPL_BY << endl;
+            do_Task_2();
+            SEP
+            break;
         default:
             cout << "Ошибка. Введите число, соответствующее пункту меню" << endl;
+            SEP
             break;
         }
     }
