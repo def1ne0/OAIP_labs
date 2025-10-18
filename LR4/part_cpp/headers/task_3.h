2993347161e842a4e207ec40eb068018ee05b868
Arseniy Finevich <clumserinri537@gmail.com>
2025-10-18 21:27:54 +0300

    updated LR(1-3)

--- LR4/part_cpp/headers/task_3.h
+++ LR4/part_cpp/headers/task_3.h
@@ -3,11 +3,11 @@
 namespace t3 {
     void do_task_3();
 
-    double** create_arr(int n);
+    double **create_arr(int);
 
-    void print_arr(double **arr, int n);
+    void print_arr(double **, int);
 
-    void print_arr(int **arr, int n);
+    void print_arr(int **, int );
 
-    int** new_arr(double **arr, int n);
+    int **new_arr(double **, int );
 }
