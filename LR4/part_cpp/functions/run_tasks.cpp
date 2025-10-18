b0dedb5ef4438bc7433f24c1a9b6e67be237afff
Arseniy Finevich <finevich03@gmail.com>
2025-10-18 18:41:19 +0300

    added LR4(1)

--- LR4/part_cpp/functions/run_tasks.cpp
+++ LR4/part_cpp/functions/run_tasks.cpp
@@ -7,8 +7,8 @@ namespace tasks {
     void run_task(
         int &choice, 
         void (*func_array[])(), 
-        const std::vector<std::string> essence, 
-        const std::string compl_by
+        const std::vector<std::string> &essence, 
+        const std::string &compl_by
     ) {
         std::cout << "Суть задания:\n " << essence[choice - 1] << compl_by << std::endl;
         func_array[choice - 1]();
--- LR4/part_cpp/functions/task_1.cpp
+++ LR4/part_cpp/functions/task_1.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 
+#include "check_cin_func.h"
 #include "task_1.h"
 
 namespace t1 {
     void do_task_1() {
-        std::cout << "pidor" << std::endl;
+        int inversions = 0;
+
+        int k = io::input_value<int>(
+            "Введите количество элементов массива k (минимум 2)", 
+            "Ошибка, попробуйте снова", 
+            true, 
+            2
+        );
+
+        int arr[k];
+
+        for (unsigned i = 0; i < k; i++) {
+            arr[i] = io::input_value<int>("Введите элемент массива с индексом " + std::to_string(i));
+        }
+
+        for (unsigned i = 0; i < k - 1; i++) {
+            for (unsigned j = i + 1; j < k; j++) {
+                if (arr[i] > arr[j]) {
+                    inversions++;
+                }
+            }
+        }
+
+        std::cout << "Число инверсий: " << inversions << std::endl;
     }
 }
