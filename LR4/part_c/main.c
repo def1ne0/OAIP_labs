3ad9fd86507f8d055008da69e6d37903abfa89c7
Arseniy Finevich <clumserinri537@gmail.com>
2025-10-23 20:21:24 +0300

    updated LR4(4)

--- LR4/part_c/main.c
+++ LR4/part_c/main.c
@@ -14,20 +14,20 @@ void menu() {
     int exit = 0;
 
     while (!exit) {
-        printf("Введите:\n 0. Для выхода\n n. Для выбора n - номера задания (4-6)\n");
+        printf("Введите:\n 0. Для выхода\n n. Для выбора n - номера задания на C (1-3)\n");
 
         void (*tasks_array[3]) () = {
             do_task_4
         };
 
-        int choice = input_int(4, 6);
+        int choice = input_int(1, 3);
 
         switch (choice) {
             case 0:
                 exit = 1;
                 break;
             default:
-                run_task(tasks_array, choice - 3);
+                run_task(tasks_array, choice);
                 break;
         }
     }
