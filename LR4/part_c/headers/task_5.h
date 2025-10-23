73221b98279074f3bc971c91c0b9ede4ee3f3885
Arseniy Finevich <clumserinri537@gmail.com>
2025-10-23 21:43:34 +0300

    updated LR4

---
+++ LR4/part_c/headers/task_5.h
@@ -0,0 +1,10 @@
+#pragma once
+
+char **create_field(int, int);
+
+void print_field(char **, int, int);
+
+char **set_numbers(char **, int, int);
+
+void do_task_5(void);
+
--- LR4/part_c/main.c
+++ LR4/part_c/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <limits.h>
 
 #include "check_input.h"
 #include "run_tasks_c.h"
 #include "task_4.h"
+#include "task_5.h"
 
 void menu(void);
 
@@ -14,21 +16,26 @@ void menu() {
     int exit = 0;
 
     while (!exit) {
-        printf("Введите:\n 0. Для выхода\n n. Для выбора n - номера задания на C (1-3)\n");
+        printf("Введите:\n 0. Для выхода\n n. Для выбора n - номера задания на C (4-6)\n");
 
         void (*tasks_array[3]) () = {
-            do_task_4
+            do_task_4,
+            do_task_5
         };
 
-        int choice = input_int(1, 3);
+        int choice = input_int(0, 6);
 
         switch (choice) {
             case 0:
                 exit = 1;
                 break;
-            default:
-                run_task(tasks_array, choice);
+            case 4:
+            case 5:
+            case 6:
+                run_task(tasks_array, choice - 3);
                 break;
+            default:
+                printf("Неверный пункт меню\n");
         }
     }
 }
