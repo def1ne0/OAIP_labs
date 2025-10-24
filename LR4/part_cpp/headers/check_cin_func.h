005f2ebbfda8de77d9ed7dcf39eb3a979728a308
Arseniy Finevich <clumserinri537@gmail.com>
2025-10-24 14:36:41 +0300

    updated LR4

--- LR4/part_cpp/headers/check_cin_func.h
+++ LR4/part_cpp/headers/check_cin_func.h
@@ -5,34 +5,6 @@
 #include <limits>
 
 namespace io {
-    template <class T> struct cin_info {
-        T arg;
-        bool is_error;
-        
-        cin_info<T>(const T& arg, bool is_error) {
-            this->arg = arg;
-            this->is_error = is_error;
-        }
-    };
-
-    template <class T> cin_info<T> check_cin() {
-        T input;
-           
-        if (std::cin >> input) {
-            cin_info<T> input_info(input, false);
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-               
-            return input_info;
-        } else {
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            cin_info<T> input_info(T{}, true);
-               
-            return input_info;
-            }
-    }
-        
     template <class T> bool check_interval_type(T arg, T a, T b) {
         T min, max;
         
@@ -48,28 +20,37 @@ namespace io {
     }
         
     template <class T> T input_value(
-        std::string input_text, std::string error_text = "Ошибка, попробуйте снова", 
+        std::string input_text, 
+        std::string error_text = "Ошибка, попробуйте снова", 
         bool interval_checking = false, 
-        T a = std::numeric_limits<T>::min(), T b = std::numeric_limits<T>::max()) {
+        T a = std::numeric_limits<T>::min(), 
+        T b = std::numeric_limits<T>::max()
+    ) {
         bool exit = false;
+        T input;
         std::cout << input_text << std::endl;
-        cin_info<T> input_info(0, 0);
-        
+
         while (!exit) {
-            input_info = check_cin<T>();
-        
-            if (input_info.is_error) {
+            if (!(std::cin >> input)) {
                 std::cout << error_text << std::endl;
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             } else if (!interval_checking) {
                 exit = true;
-            } else if (check_interval_type(input_info.arg, a, b)) {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            } else if (check_interval_type(input, a, b)) {
                 exit = true;
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             } else {
                 std::cout << "Выход из диапазона данного типа" << std::endl;
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             }
         
         }
         
-        return input_info.arg;
+        return input;
     }
 }
