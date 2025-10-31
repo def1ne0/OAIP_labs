2b1b70b719ca28d434b6c9d1de56f7e51805bb72
Arseniy Finevich <clumserinri537@gmail.com>
2025-10-31 23:19:34 +0300

    updated LR4

--- LR4/part_cpp/headers/run_tasks.h
+++ LR4/part_cpp/headers/run_tasks.h
@@ -1,13 +1,13 @@
 #pragma once
 
-#include <iostream>
+#include <string>
 #include <vector>
 
 namespace tasks {
     void run_task(
-        int &, 
-        void (*[]) (), 
-        const std::vector<std::string> &, 
+        int &,
+        void (*[]) (),
+        const std::vector<std::string> &,
         const std::string &
     );
 }
