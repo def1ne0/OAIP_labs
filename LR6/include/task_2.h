f0adc5a460193ff5d25e3248d82b5ed16b72a7df
Arseniy Finevich <finevich03@gmail.com>
2025-11-25 21:23:54 +0300

    added LR6(2)

---
+++ LR6/include/task_2.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <stddef.h>
+#include "MyStr.h"
+
+namespace task_2 {
+    size_t find_biggest(int *arr, size_t size);
+
+    void execute_task_2(str::MyStr text);
+
+    void execute_task_2(char *text);
+
+    void do_task_2();
+}
