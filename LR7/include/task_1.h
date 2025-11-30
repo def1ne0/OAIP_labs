63cef4cc836bbafe1ce808634a655c4d85744592
Arseniy Finevich <finevich03@gmail.com>
2025-11-30 18:47:16 +0300

    updated LR8

---
+++ LR7/include/task_1.h
@@ -0,0 +1,8 @@
+#ifndef TASK_1_H
+#define TASK_1_H
+
+namespace task_1 {
+    void do_task_1();
+}
+
+#endif // TASK_1_H
---
+++ LR7/src/run_tasks.cpp
@@ -0,0 +1,15 @@
+#include <print>
+
+#include "run_tasks.h"
+
+namespace tasks {
+    void run_task(
+        int &choice,
+        void (*func_array[])()
+    ) {
+        func_array[choice - 1]();
+        std::println("*********************");
+
+        return;
+    }
+}
