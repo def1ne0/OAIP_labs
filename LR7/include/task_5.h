4efda8a07ee4bb6d90daa71a731bdded98725170
Arseniy Finevich <finevich03@gmail.com>
2025-12-13 01:05:07 +0300

    updated LR7

--- LR7/include/task_5.h
+++ LR7/include/task_5.h
@@ -1,7 +1,13 @@
 #ifndef TASK_5_H
 #define TASK_5_H
 
+#include <vector>
+
 namespace task_5 {
+    std::vector<int> to_ternary(int n);
+
+    int get_result(std::vector<int> res_vec);
+
     void do_task_5();
 }
 
