56bac9c068a532095b2896ea77c060ad0223a418
Arseniy Finevich <clumserinri537@gmail.com>
2025-12-13 12:52:32 +0300

    updated LR8(1)

--- LR8/include/airport.h
+++ LR8/include/airport.h
@@ -5,7 +5,38 @@
 extern "C" {
 #endif
 
-void xyi(void);
+#include <stdbool.h>
+
+typedef struct {
+    int flight_number;      // Номер рейса (целое число)
+    char aircraft_type[50]; // Тип самолета (строка)
+    char destination[100];  // Пункт назначения (строка)
+    int departure_time;     // Время вылета в минутах с начала суток (целое число)
+
+    union {
+        int time_minutes;   // Время в минутах (как в departure_time)
+        char time_string[10]; // Время в формате "HH:MM" (например, "13:50")
+    } time_data;
+} Flight;
+
+// *flights - указатель на первый элемент массива flights
+// **flights - указатель на указатель на первый элемент массива flights, чтобы изменять массив исходный,
+// ведь Flight *flights копируется
+
+void init_flight_array(Flight **flights, int *size);
+void free_flight_array(Flight **flights);
+void input_flights(Flight **flights, int *size);
+void display_flights(Flight *flights, int size);
+void add_flight(Flight **flights, int *size);
+void search_and_display_by_destination(Flight *flights, int size);
+void delete_flight_by_number(Flight **flights, int *size);
+void update_flight_by_number(Flight *flights, int size);
+void sort_flights_by_departure_time(Flight *flights, int size);
+void print_menu();
+
+void save_flights_to_binary_file(Flight *flights, int size, const char *filename);
+void load_flights_from_binary_file(Flight **flights, int *size, const char *filename);
+void correct_flight_in_binary_file(const char *filename, int flight_number_to_correct);
 
 #ifdef __cplusplus
 }
--- LR8/src/tasks/task_1.c
+++ LR8/src/tasks/task_1.c
@@ -1,7 +1,34 @@
+#include <stdio.h>
+
 #include "airport.h"
 
-int main(void) {
-    xyi();
+int main() {
+    Flight *flights = NULL;
+    int size = 0;
+    int choice;
+
+    init_flight_array(&flights, &size);
+
+    do {
+        print_menu();
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                input_flights(&flights, &size);
+                break;
+            case 2:
+                display_flights(flights, size);
+                break;
+            case 0:
+                printf("Выход из программы.\n");
+                break;
+            default:
+                printf("Неверный выбор. Попробуйте снова.\n");
+        }
+    } while (choice != 0);
+
+    free_flight_array(&flights);
 
     return 0;
 }
